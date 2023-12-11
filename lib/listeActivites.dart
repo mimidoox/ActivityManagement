79424997e3b6bf8c344e8e7161ebd489e5b5ded7
ARRAJ Mohamed <118699556+mimidoox@users.noreply.github.com>
2023-12-11 13:06:54 +0100

    US#4 filter by Category

--- lib/listeActivites.dart
+++ lib/listeActivites.dart
@@ -1,71 +1,141 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-class ListeActivites extends StatelessWidget {
+class ListeActivites extends StatefulWidget {
+  @override
+  _ListeActivitesState createState() => _ListeActivitesState();
+}
+
+class _ListeActivitesState extends State<ListeActivites> {
+  String selectedCategory = 'All';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('activites').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          var activites = snapshot.data!.docs;
-
-          return ListView.builder(
-            itemCount: activites.length,
-            itemBuilder: (context, index) {
-              var activite = activites[index].data() as Map<String, dynamic>;
-
-              return GestureDetector(
-                onTap: () {
-                  _showDetailsDialog(context, activite);
-                },
-                child: Card(
-                  elevation: 3,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // Afficher l'image de l'activité à partir de l'URL
-                      Image.network(
-                        activite['image'],
-                        width: 150,
-                        height: 150,
-                        fit: BoxFit.cover,
-                      ),
-                      SizedBox(width: 16),
-                      Expanded(
-                        child: ListTile(
-                          contentPadding: EdgeInsets.all(16),
-                          title: Text(activite['titre']),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(height: 8),
-                              Text(
-                                  '${activite['prix']}€ - ${activite['lieu']}'),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        },
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildCategoriesList(),
+          Expanded(
+            child: _buildActivitiesList(selectedCategory),
+          ),
+        ],
       ),
     );
   }
 
-  // Fonction pour afficher les détails dans une boîte de dialogue
+  Widget _buildCategoriesList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('activites').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return CircularProgressIndicator();
+        }
+
+        var categories = snapshot.data!.docs;
+        Set<String> uniqueCategories = {'All'};
+
+        for (var category in categories) {
+          uniqueCategories.add(category['categorie']);
+        }
+
+        return Container(
+          height: 50,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              for (var category in uniqueCategories)
+                _buildCategoryItem(category),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildCategoryItem(String category) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedCategory = category;
+          });
+        },
+        child: Chip(
+          label: Text(category),
+          backgroundColor: selectedCategory == category
+              ? Colors.blue
+              : Theme.of(context).chipTheme.backgroundColor,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildActivitiesList(String categoryFilter) {
+    Query activitiesQuery = FirebaseFirestore.instance.collection('activites');
+
+    if (categoryFilter != 'All') {
+      activitiesQuery =
+          activitiesQuery.where('categorie', isEqualTo: categoryFilter);
+    }
+
+    return StreamBuilder(
+      stream: activitiesQuery.snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        var activites = snapshot.data!.docs;
+
+        return ListView.builder(
+          itemCount: activites.length,
+          itemBuilder: (context, index) {
+            var activite = activites[index].data() as Map<String, dynamic>;
+
+            return GestureDetector(
+              onTap: () {
+                _showDetailsDialog(context, activite);
+              },
+              child: Card(
+                elevation: 3,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.network(
+                      activite['image'],
+                      width: 150,
+                      height: 150,
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: ListTile(
+                        contentPadding: EdgeInsets.all(16),
+                        title: Text(activite['titre']),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(height: 8),
+                            Text('${activite['prix']}€ - ${activite['lieu']}'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
   void _showDetailsDialog(BuildContext context, Map<String, dynamic> activite) {
     showDialog(
       context: context,
@@ -78,10 +148,9 @@ class ListeActivites extends StatelessWidget {
             children: [
               Image.network(
                 activite['image'],
-                width: 200, // Ajustez la largeur selon vos besoins
-                height: 200, // Ajustez la hauteur selon vos besoins
-                fit: BoxFit
-                    .cover, // Ajustez le mode d'ajustement selon vos besoins
+                width: 200,
+                height: 200,
+                fit: BoxFit.cover,
               ),
               SizedBox(height: 16),
               Text('${activite['titre']}'),
@@ -94,7 +163,6 @@ class ListeActivites extends StatelessWidget {
                   '- le nombre minimal des personnes est ${activite['nbrMin']}'),
               SizedBox(height: 8),
               Text('- le prix du ticket est ${activite['prix']}€ par heure'),
-              // Ajoutez d'autres champs ici
             ],
           ),
         );
